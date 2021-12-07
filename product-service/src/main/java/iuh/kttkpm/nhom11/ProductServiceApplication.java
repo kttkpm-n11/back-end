7fcf0252c9ac71a7f77780f5fe11dad4ea58e946
maikiencuong <maikiencuongiuh@gmail.com>
2021-12-07 08:37:32 +0700

    [master] config swagger -- swagger ui - http://localhost:9000/products/docs/swagger-ui/ - http://localhost:9000/suppliers/docs/swagger-ui/
    -- api
    - http://localhost:9000/api/products
    - http://localhost:9000/api/suppliers

--- product-service/src/main/java/iuh/kttkpm/nhom11/ProductServiceApplication.java
+++ product-service/src/main/java/iuh/kttkpm/nhom11/ProductServiceApplication.java
@@ -10,7 +10,13 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @EnableEurekaClient
 @SpringBootApplication
 public class ProductServiceApplication implements CommandLineRunner {
@@ -42,4 +48,17 @@ public class ProductServiceApplication implements CommandLineRunner {
             productRepository.save(product);
         }
     }
+
+    /*
+    config swagger, generate document for api
+    */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("iuh.kttkpm.nhom11"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
 }
--- supplier-service/src/main/java/iuh/kttkpm/nhom11/SupplierServiceApplication.java
+++ supplier-service/src/main/java/iuh/kttkpm/nhom11/SupplierServiceApplication.java
@@ -7,7 +7,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @EnableEurekaClient
 @SpringBootApplication
 public class SupplierServiceApplication implements CommandLineRunner {
@@ -32,4 +39,17 @@ public class SupplierServiceApplication implements CommandLineRunner {
             supplierRepository.save(supplier);
         }
     }
+
+    /*
+    config swagger, generate document for api
+     */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("iuh.kttkpm.nhom11"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
 }
